023db4d04c2d4daa3138d4e1a7994e65624f904a
Mark Barr <mark@hyperingenuity.com>
2024-10-24 17:41:14 -0400

    certificate-checker: improved cli output

--- cli.js
+++ cli.js
@@ -3,17 +3,38 @@
 
 const async = require('async');
 const { validate } = require('./index');
+const process = require('node:process');
+const { styleText } = require('node:util');
+const parser = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
+
 const { version } = require('./package.json');
 
+function formatter (results) {
+  for (const result of results) {
+    const icon = result.valid ? styleText('green', ' \u2714 ') : styleText('red', ' \u2716 ');
+    const valid = result.valid ? styleText('green', 'ok') : styleText('red', 'invalid');
+    const reasons = result.reasons.length ? styleText('red', ` ${ result.reasons.join(', ') }`) : '';
+    console.log(`${ icon }${ result.location } ${ valid }${ reasons }`);
+    if (!result.valid) {
+      process.exitCode = 1;
+    }
+  }
+}
+
 async function main () {
-  await require('yargs/yargs')(hideBin(process.argv)).
+  await parser(hideBin(process.argv)).
     command('check <locations...>', 'Check the certificates at the given location(s).',
       (yargs) => yargs.positional('locations', { describe: 'URLs or local filesystem paths' }),
       async (argv) => {
         const results = await async.map(argv.locations, validate);
-        console.log(results);
+        return formatter(results, argv);
       }).
+    option('verbose', {
+      alias: 'v',
+      type: 'boolean',
+      description: 'Verbose output',
+    }).
     strictCommands().
     demandCommand(1).
     help().
